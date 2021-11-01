53d21a598d3b4069bfa232bf64a4ce38d8b98493
Leonardo <leonoman07@gmail.com>
2021-11-01 07:51:17 -0300

    Criado componentes inciais e definição de rotas

--- teste-zssn/src/App.js
+++ teste-zssn/src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Trade from "./components/Trade";
+import Localization from "./components/Localization";
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/localizacao" exact>
+          <Localization />
+        </Route>
+        <Route path="/escambo" exact>
+          <Trade />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
