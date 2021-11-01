cde51f900d07140140b28312be7083c7e402aec9
Leonardo <leonoman07@gmail.com>
2021-11-01 07:56:09 -0300

    Adicionado tela inicial e seu respectivo estilo

--- teste-zssn/src/components/Home/index.jsx
+++ teste-zssn/src/components/Home/index.jsx
@@ -1,7 +1,23 @@
+import "./styles.css";
+
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home-container">
+      <h1>Zombie Survival Social Network</h1>
+      <div className="home-links-container">
+        <Link to="/localizacao">
+          <div className="home-localization-container">
+            <span>Localização</span>
+          </div>
+        </Link>
+        <Link to="/escambo">
+          <div className="home-trade-container">
+            <span>Escambo</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
